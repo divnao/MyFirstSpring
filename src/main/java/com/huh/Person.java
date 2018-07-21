4e17986c49059d3339451c84ef8688519b1a06eb
19800 <hao.hu06@hand-china.com>
2018-07-21 08:54:07 +0800

    v0.5

---
+++ src/main/java/com/huh/Person.java
@@ -0,0 +1,29 @@
+package com.huh;
+
+public class Person {
+
+    private int age;
+
+    private String name;
+
+    static {
+        System.out.println("父类静态代码块");
+    }
+
+    public Person(){
+        System.out.println("父类构造方法");
+    }
+
+}
+
+class Student extends Person{
+    static {
+        System.out.println("子类静态代码块");
+    }
+
+
+    public Student(){
+        System.out.println("子类构造方法");
+
+    }
+}
