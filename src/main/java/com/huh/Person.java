91a98cdddbf19a0513d9e88e137235afea3347a8
19800 <hao.hu06@hand-china.com>
2018-07-21 14:52:22 +0800

    v0.6, 个人事物管理v0.1

--- src/main/java/com/huh/Person.java
+++ src/main/java/com/huh/Person.java
@@ -1,19 +1,20 @@
 package com.huh;
 
 public class Person {
-
     private int age;
-
     private String name;
 
     static {
         System.out.println("父类静态代码块");
     }
 
+    {
+        System.out.println("父类代码块");
+    }
+
     public Person(){
         System.out.println("父类构造方法");
     }
-
 }
 
 class Student extends Person{
@@ -21,9 +22,11 @@ class Student extends Person{
         System.out.println("子类静态代码块");
     }
 
+    {
+        System.out.println("子类代码块");
+    }
 
     public Student(){
         System.out.println("子类构造方法");
-
     }
 }
